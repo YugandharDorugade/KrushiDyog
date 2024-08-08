68bccb20005f35a60f9d2269fe354d3e3e7ad23a
YugandharDorugade <yugandhardorugade@yugandhars-MacBook-Air.local>
2024-08-08 22:30:05 +0530

    listings intialized

--- init/index.js
+++ init/index.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
-
+require('dotenv').config({ path: '../.env' }); // Load environment variables
 
 const dburl = process.env.ATLASDB_URL;
 
-
-
-
 async function main() {
   try {
+    console.log("Database URL:", dburl); // Check the database URL
     await mongoose.connect(dburl);
     console.log("connected to DB");
   } catch (err) {
